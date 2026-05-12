b56999668bdb5fc4bc952e85d537fbbb3bb8782d
Abhidhya-Shanmugam <abhidhyashanmugam@gmail.com>
2026-05-12 12:15:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function addTask() {
+
+    let input = document.getElementById("taskInput");
+
+    let task = input.value;
+
+    if (task === "") {
+
+        alert("Please enter a task!");
+
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+
+        <span class="task-text" onclick="completeTask(this)">
+            ${task}
+        </span>
+
+        <button class="delete-btn" onclick="deleteTask(this)">
+            Delete
+        </button>
+
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function deleteTask(button) {
+
+    button.parentElement.remove();
+}
+
+function completeTask(task) {
+
+    task.classList.toggle("completed");
+}
